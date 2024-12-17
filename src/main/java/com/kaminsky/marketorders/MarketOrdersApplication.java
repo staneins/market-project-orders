65d2dd6d6ab82bca35bbddb78f97dd58de01f868
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-17 16:41:06 +0300

    initial commit

---
+++ src/main/java/com/kaminsky/marketorders/MarketOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.kaminsky.marketorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketOrdersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaminsky/marketorders/MarketOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminsky.marketorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
